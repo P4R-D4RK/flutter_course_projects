67f26b9792e7674a3d9b84fd91a74effb9ca6ffc
Luis Polina <luis.282001@gmail.com>
2023-08-09 15:51:17 -0600

    Start screen

---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key, required this.text, required this.image});
+
+  final String text;
+  final String image;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            image,
+            width: 300,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Text(
+            text,
+            style: const TextStyle(color: Colors.white, fontSize: 28),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start Quiz'))
+        ],
+      ),
+    );
+  }
+}
